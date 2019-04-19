9233e1dbbce125f849289bdb9adec21f478d59e3
Felix Brucker <felix@felixbrucker.com>
2019-04-19 22:22:58 +0200

    Use 2 as default precision for capacities

--- shared/capacity.js
+++ shared/capacity.js
@@ -7,7 +7,7 @@ class Capacity {
         return new Capacity(capacityInTiB * 1024);
     }
 
-    toString(precision = 0, correctUnit = true) {
+    toString(precision = 2, correctUnit = true) {
         let capacity = this.capacityInGiB;
         let unit = 0;
         const units = correctUnit ? ['GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'] : ['GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
