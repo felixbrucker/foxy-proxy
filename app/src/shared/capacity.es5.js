041a84676ec3552c9e59746468cbfcb6d2725c42
Felix Brucker <felix@felixbrucker.com>
2020-04-03 17:39:42 +0200

    Update web-ui dist

--- app/src/shared/capacity.es5.js
+++ app/src/shared/capacity.es5.js
@@ -10,9 +10,7 @@ function _defineProperties(target, props) { for (var i = 0; i < props.length; i+
 
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-var Capacity =
-/*#__PURE__*/
-function () {
+var Capacity = /*#__PURE__*/function () {
   function Capacity(capacityInGiB) {
     _classCallCheck(this, Capacity);
 
